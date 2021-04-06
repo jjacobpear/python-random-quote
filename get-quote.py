b98f87f822346191233eafa8b3ffe9b51d5cce94
jjacobpear <82059694+jjacobpear@users.noreply.github.com>
2021-04-06 22:11:01 +0100

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
-def focus():
-   print("Keep it logically awesome.")
+def main():
+  
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
-  focus()
+  main()
